c0c5732d158a6cece7f522236b260892e1ca89aa
Ajay Andrew <ajayandrew18@gmail.com>
2026-06-08 18:54:02 +0530

    added vercel.json & modified vite.config file

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: { include: ['framer-motion', 'lucide-react'] }
+  optimizeDeps: {
+    include: ['framer-motion', 'lucide-react']
+  },
+  build: {
+    target: 'es2015'
+  }
 })
